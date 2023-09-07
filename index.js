6158e2800de33c2fcbdcdef41801037cad23e942
deathworship <125945145+deathworship@users.noreply.github.com>
2023-09-08 00:47:47 +0300

    front dist

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ let notes = [
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
